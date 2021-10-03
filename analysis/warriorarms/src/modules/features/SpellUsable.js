3cf652e8e5ce11985209eb893565f37219850fee
elasticspoon <quesadillaman@gmail.com>
2021-10-03 18:08:56 -0400

    fixed for wonky overpower behavior

--- analysis/warriorarms/src/modules/features/SpellUsable.js
+++ analysis/warriorarms/src/modules/features/SpellUsable.js
@@ -1,19 +1,25 @@
 import SPELLS from 'common/SPELLS';
 import { EventType } from 'parser/core/Events';
+import Abilities from 'parser/core/modules/Abilities';
 import CoreSpellUsable from 'parser/shared/modules/SpellUsable';
 
 class SpellUsable extends CoreSpellUsable {
   static dependencies = {
     ...CoreSpellUsable.dependencies,
+    abilities: Abilities,
   };
-
   onEvent(event) {
     super.onEvent(event);
     // Tactician passive: You have a 1.40% chance per Rage spent on damaging abilities to reset the remaining cooldown on Overpower.
+    // normally charges dont count down simultaneously. these do
     if (event.type === EventType.ApplyBuff || event.type === EventType.RefreshBuff) {
       if (event.ability.guid === SPELLS.TACTICIAN.id) {
         if (this.isOnCooldown(SPELLS.OVERPOWER.id)) {
-          this.endCooldown(SPELLS.OVERPOWER.id);
+          const remainingCD =
+            this.abilities.getAbility(SPELLS.OVERPOWER.id).cooldown * 1000 -
+            this.cooldownRemaining(SPELLS.OVERPOWER.id);
+          this.log(`${remainingCD} left on OP CD.`);
+          this.endCooldown(SPELLS.OVERPOWER.id, false, this.owner.currentTimestamp, remainingCD);
         }
       }
     }
